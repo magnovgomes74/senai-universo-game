471bbb2055f1f308f3b6025f8408e132d3902c57
Magno V Gomes <magnovgomes74@gmail.com>
2025-11-19 20:18:00 -0300

    Finalizando a atividade 2 da situação-problema 2 da UC09

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 //efeito do botão voltar ao Topo
 
+function topo(){
+    window.scrollTo(
+        {
+            top:0,
+            left:0,
+            behavior:'smooth'
+        }
+    )
+}
+
 //Validação de Login
 
-//Ativar alert no botão cadastrar
+function login(){
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if(usuario == 'admin' && senha == '123456'){
+        window.location = "index.html";
+        logado = 1;
+    }
+    else{
+        alert("Acesso Negado. Dados incorretos");
+function cadastro() {
+    alert("Cadastrado com sucesso!");
+    window.location.href = "index.html";
+}
